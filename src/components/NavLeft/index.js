4f0c3ae501ae5704694b1e77a9f1faa88e026e5a
lixiaofeng <1130034920@qq.com>
2019-07-01 23:47:39 +0800

    3-7 菜单组件开发（2）

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -5,6 +5,21 @@ import './index.less'
 const { SubMenu } = Menu;
 
 export default class NavLeft extends Component {
+    componentWillMount() {
+        const menuTreeNode = this.renderMenu(MenuConfig)
+        this.setState({ menuTreeNode })
+    }
+    // 菜单渲染
+    renderMenu = (data) => {
+        return data.map((item) => {
+            if (item.children) {
+                return <SubMenu title={item.title} key={item.key}>
+                    {this.renderMenu(item.children)}
+                </SubMenu>
+            }
+            return <Menu.Item key={item.key}>{item.title}</Menu.Item>
+        })
+    }
     render() {
         return (
             <div>
@@ -13,10 +28,7 @@ export default class NavLeft extends Component {
                     <h1>Imooc MS</h1>
                 </div>
                 <Menu theme="dark">
-                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                        <Menu.Item key="1">Option 1</Menu.Item>
-                        <Menu.Item key="2">Option 2</Menu.Item>
-                    </SubMenu>
+                    {this.state.menuTreeNode}
                 </Menu>
             </div>
         )
