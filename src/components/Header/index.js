6cb98d522d5ac06c63dce1bf1aa0aa4e55da7359
lixiaofeng <1130034920@qq.com>
2019-07-02 00:33:34 +0800

    3-7 头部组件实现（1）

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+import './index.less'
+import Util from '../../utils/utils'
 export default class Header extends Component {
+    componentWillMount() {
+        this.setState({
+            userName: '河畔一角',
+            sysTime: ''
+        })
+        setInterval(() => {
+            let sysTime = Util.formatDate(new Date().getTime())
+            this.setState({ sysTime })
+        }, 1000)
+    }
     render() {
         return (
-           <div>this is Header</div>
+            <div className="header">
+                <Row className="header-top">
+                    <Col span={24}>
+                        <span>欢迎，{this.state.userName}</span>
+                        <a href="#">退出</a>
+                    </Col>
+                </Row>
+                <Row className="breadcrumb">
+                    <Col span={4} className="breadcrumb-title">
+                        首页
+                    </Col>
+                    <Col span={20} className="weather">
+                        <span className="date">{this.state.sysTime}</span>
+                        <span className="weather-detail">晴转多云</span>
+                    </Col>
+                </Row>
+            </div>
         )
     }
 }
