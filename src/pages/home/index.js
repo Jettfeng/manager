19e36fb090b9859927f2e7544ae2dd120faad302
lixiaofeng <1130034920@qq.com>
2019-07-03 22:13:47 +0800

    3-9 底部组件功能实现（1）

---
+++ src/pages/home/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import './index.less'
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="home-wrap">欢迎学IMooc后台管理系统</div>
+        )
+    }
+}
