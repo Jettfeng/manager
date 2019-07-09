3d62da7bd0e4f6b17c311edaf222499668888ccb
lixiaofeng <1130034920@qq.com>
2019-07-09 23:07:49 +0800

    5-5 Loading组件使用

---
+++ src/pages/ui/loadings.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Card, Button, Spin, Icon, Alert } from 'antd';
+import './ui.less'
+export default class Loadings extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loading: true,
+            size: 'default'
+        }
+    }
+
+    render() {
+        const icon = <Icon type="loading" style={{ fontSize: 24 }} />
+        const iconLoading = <Icon type="loading" style={{ fontSize: 24 }} />
+        return (
+            <div>
+                <Card title="Spin的用法" className="card-wrap">
+                    <Spin size='small' />
+                    <Spin style={{ margin: '0 10px' }} />
+                    <Spin size='large' />
+                    <Spin indicator={icon} style={{ marginLeft: 10 }} />
+                </Card>
+                <Card title="内容遮罩" className="card-wrap">
+                    <Alert message="React" description="欢迎来学习React" type="info" />
+                    <Alert message="React" description="欢迎来学习React" type="warning" />
+                    <Spin>
+                        <Alert message="React" description="欢迎来学习React" type="warning" />
+                    </Spin>
+                    <Spin tip="加载中...">
+                        <Alert message="React" description="欢迎来学习React" type="warning" />
+                    </Spin>
+                    <Spin tip="加载中..." indicator={iconLoading}>
+                        <Alert message="React" description="欢迎来学习React" type="warning" />
+                    </Spin>
+                </Card>
+            </div>
+        )
+    }
+}
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Login from './pages/login'
 import Admin from './admin'
 import Buttons from './pages/ui/buttons.js'
 import Modals from './pages/ui/modals'
+import Loadings from './pages/ui/loadings'
 import Nomatch from './pages/nomatch'
 export default class IRouter extends Component {
     render() {
@@ -17,6 +18,7 @@ export default class IRouter extends Component {
                             <Switch>
                                 <Route path="/ui/buttons" component={Buttons} />
                                 <Route path="/ui/modals" component={Modals} />
+                                <Route path="/ui/loadings" component={Loadings} />
                                 <Route component={Nomatch} />
                             </Switch>
                         </Admin>
