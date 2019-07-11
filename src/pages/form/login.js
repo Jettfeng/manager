56bdf6c32ff58dfa5658bcc88f127904da0f25ed
lixiaofeng <1130034920@qq.com>
2019-07-12 00:35:29 +0800

    6-2 登录表单讲解（下）

--- src/pages/form/login.js
+++ src/pages/form/login.js
@@ -5,8 +5,17 @@ class FormLogin extends React.Component {
     constructor(props) {
         super(props)
     }
+    handleSubmit = () => {
+        let userInfo = this.props.form.getFieldsValue()
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                message.success(`userName：${userInfo.userName},pwd：${userInfo.userPwd}`)
+            }
+        })
+        console.log(userInfo);
+    }
     render() {
-        const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form
+        const { getFieldDecorator } = this.props.form
         return (
             <div>
                 <Card title="登录行内表单">
@@ -27,21 +36,45 @@ class FormLogin extends React.Component {
                         <FormItem>
                             {
                                 getFieldDecorator('userName', {
-                                    initialValue: 'Jack',
-                                    reles: []
+                                    initialValue: '',
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: '用户名不能为空'
+                                        },
+                                        {
+                                            min: 5, max: 10,
+                                            message: '长度不在范围内'
+                                        },
+                                        {
+                                            pattern: new RegExp('^\\w+$', 'g'),
+                                            message: '用户名必须为字母或者数字'
+                                        }
+                                    ]
                                 })(<Input prefix={<Icon type="user" />} placeholder="晴输入用户名" />)
                             }
                         </FormItem>
                         <FormItem>
                             {
-                                getFieldDecorator('password', {
+                                getFieldDecorator('userPwd', {
                                     initialValue: '123456',
                                     reles: []
-                                })(<Input prefix={<Icon type="lock" />} type="password" placeholder="晴输入用户名" />)
+                                })(<Input prefix={<Icon type="lock" />} type="password" placeholder="晴输入密码" />)
                             }
                         </FormItem>
                         <FormItem>
-                            <Button type="primary" style={{ width: 300 }}>登录</Button>
+                            {
+                                getFieldDecorator('remember', {
+                                    valuePropName: 'checked',
+                                    initialValue: true
+                                })(
+                                    <Checkbox>记住密码</Checkbox>
+                                )
+                            }
+                            <a href="#" style={{ float: 'right' }}>忘记密码</a>
+                        </FormItem>
+                        <FormItem>
+                            <Button type="primary" onClick={this.handleSubmit} style={{ width: 300 }}>登录</Button>
                         </FormItem>
                     </Form>
                 </Card>
