90a826214cc70acf4eaf3899527323e4dc93915a
lixiaofeng <1130034920@qq.com>
2019-07-14 12:11:20 +0800

    7-3 项目工程化之表格动态渲染（2）

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,4 +1,6 @@
+import axios from 'axios'
 import JsonP from 'jsonp'
+import { Modal } from 'antd';
 export default class Axios {
     static jsonp(options) {
         return new Promise((resolve, reject) => {
@@ -9,6 +11,32 @@ export default class Axios {
                     resolve(response)
                 } else {
                     reject(response.message)
+                }
+            })
+        })
+    }
+    static ajax(options) {
+        const baesApi = 'https://easy-mock.com/mock/5d2a9caa1bba03305e100ec5/api'
+        return new Promise((resolve, reject) => {
+            axios({
+                url: options.url,
+                method: 'get',
+                baseURL: baesApi,
+                timeout: 5000,
+                params: (options.data && options.data.params) || ''
+            }).then((response) => {
+                if (response.status === 200) {
+                    let res = response.data
+                    if (res.code === 0) {
+                        resolve(res)
+                    } else {
+                        Modal.info({
+                            title: "",
+                            content: res.msg
+                        })
+                    }
+                } else {
+                    reject(response.data)
                 }
             })
         })
--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, Table, Modal, Button, message } from 'antd'
-import axios from 'axios'
+import axios from '../../axios/index'
 export default class BasicTable extends React.Component {
     constructor(props) {
         super(props)
@@ -47,12 +47,16 @@ export default class BasicTable extends React.Component {
         this.request()
     }
     request = () => {
-        axios({
-            methods: 'get',
-            url: 'https://easy-mock.com/mock/5d2a9caa1bba03305e100ec5/api/table/list'
+        axios.ajax({
+            url: '/table/list',
+            data: {
+                params: {
+                    page: 1
+                }
+            }
         }).then((res) => {
-            if (res.status === 200 && res.data.code === 0) {
-                let dataSource2 = res.data.result
+            if (res.code === 0) {
+                let dataSource2 = res.result
                 dataSource2.forEach((item, index) => {
                     item.key = index
                 })
