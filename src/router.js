a234b764a352b8d320f80a63087b05270b7e3f29
lixiaofeng <1130034920@qq.com>
2019-07-15 22:36:39 +0800

    7-8and7-9 高级表格

--- src/router.js
+++ src/router.js
@@ -14,6 +14,7 @@ import Carousel from './pages/ui/carousel'
 import FormLogin from './pages/form/login'
 import FormRegister from './pages/form/register'
 import BasicTable from './pages/table/basicTable'
+import HighTable from './pages/table/highTable'
 import Nomatch from './pages/nomatch'
 export default class IRouter extends Component {
     render() {
@@ -35,6 +36,7 @@ export default class IRouter extends Component {
                                 <Route path="/form/login" component={FormLogin} />
                                 <Route path="/form/reg" component={FormRegister} />
                                 <Route path="/table/basic" component={BasicTable} />
+                                <Route path="/table/high" component={HighTable} />
                                 <Route component={Nomatch} />
                             </Switch>
                         </Admin>
