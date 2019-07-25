a786f3189508aa6c18d90079d8c1c9eef402dab4
lixiaofeng <1130034920@qq.com>
2019-07-25 23:43:51 +0800

    12-2 地图数据渲染实现

--- src/pages/map/bikeMap.js
+++ src/pages/map/bikeMap.js
@@ -60,7 +60,82 @@ export default class BikeMap extends React.Component {
         let endPoint = new window.BMap.Point(gps2[0], gps2[1]);
 
         this.map.centerAndZoom(endPoint, 11);
+
+        //添加起始图标
+        let startPointIcon = new window.BMap.Icon("/assets/start_point.png", new window.BMap.Size(36, 42), {
+            imageSize: new window.BMap.Size(36, 42),
+            anchor: new window.BMap.Size(18, 42)
+        });
+        var bikeMarkerStart = new window.BMap.Marker(startPoint, { icon: startPointIcon });
+        this.map.addOverlay(bikeMarkerStart);
+
+        //添加终点图标
+        let endPointIcon = new window.BMap.Icon("/assets/end_point.png", new window.BMap.Size(36, 42), {
+            imageSize: new window.BMap.Size(36, 42),
+            anchor: new window.BMap.Size(18, 42)
+        });
+        var bikeMarkerEnd = new window.BMap.Marker(endPoint, { icon: endPointIcon });
+        this.map.addOverlay(bikeMarkerEnd);
+
+        // 行驶路线
+        let routeList = [];
+        list.forEach((item) => {
+            let p = item.split(",");
+            let point = new window.BMap.Point(p[0], p[1]);
+            routeList.push(point);
+        })
+
+        var polyLine = new window.BMap.Polyline(routeList, {
+            strokeColor: "#ef4136",
+            strokeWeight: 3,
+            strokeOpacity: 1
+        });
+        this.map.addOverlay(polyLine);
+
+        // 服务区路线
+        let serviceList = res.service_list;
+        serviceList.push(serviceList[0])
+        let servicePointist = [];
+        serviceList.forEach((item) => {
+            let point = new window.BMap.Point(item.lon, item.lat);
+            servicePointist.push(point);
+        })
+        // 画线
+        var polyServiceLine = new window.BMap.Polyline(servicePointist, {
+            strokeColor: "#ef4136",
+            strokeWeight: 3,
+            strokeOpacity: 1
+        });
+        this.map.addOverlay(polyServiceLine);
+
+        // 添加地图中的自行车
+        let bikeList = res.bike_list;
+        let bikeIcon = new window.BMap.Icon("/assets/bike.jpg", new window.BMap.Size(36, 42), {
+            imageSize: new window.BMap.Size(36, 42),
+            anchor: new window.BMap.Size(18, 42)
+        });
+        bikeList.forEach((item) => {
+            let p = item.split(",");
+            let point = new window.BMap.Point(p[0], p[1]);
+            var bikeMarker = new window.BMap.Marker(point, { icon: bikeIcon });
+            this.map.addOverlay(bikeMarker);
+        })
+
+        // 添加地图控件
+        this.addMapControl();
     }
+    // 添加地图控件
+    addMapControl = () => {
+        let map = this.map;
+        // 左上角，添加比例尺
+        var top_right_control = new window.BMap.ScaleControl({ anchor: window.BMAP_ANCHOR_TOP_RIGHT });
+        var top_right_navigation = new window.BMap.NavigationControl({ anchor: window.BMAP_ANCHOR_TOP_RIGHT });
+        //添加控件和比例尺
+        map.addControl(top_right_control);
+        map.addControl(top_right_navigation);
+        map.enableScrollWheelZoom(true);
+        // legend.addLegend(map);
+    };
     render() {
         return (
             <div>
