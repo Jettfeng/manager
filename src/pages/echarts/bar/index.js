0d77c16c3fb8b5d6fc5360b18d2613eda1bc00ef
lixiaofeng <1130034920@qq.com>
2019-07-28 22:44:48 +0800

    13-2 柱形图功能实现

--- src/pages/echarts/bar/index.js
+++ src/pages/echarts/bar/index.js
@@ -13,11 +13,82 @@ import 'echarts/lib/component/title';
 import 'echarts/lib/component/legend';
 import 'echarts/lib/component/markPoint';
 export default class Bar extends React.Component {
-
+    constructor(props) {
+        super(props)
+    }
+    componentWillMount() {
+        echarts.registerTheme('Imooc', echartTheme) //注入主题：需要提前注入
+    }
+    getOption = () => {
+        let option = {
+            title: {
+                text: '用户骑行订单'
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            xAxis: {
+                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    name: "订单量",
+                    type: 'bar',
+                    data: [1000, 2000, 1500, 3000, 2000, 1200, 800]
+                }
+            ]
+        }
+        return option
+    }
+    getOption2 = () => {
+        let option = {
+            title: {
+                text: '用户骑行订单'
+            },
+            legend: {
+                data: ['OFO', '摩拜', '小蓝']
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            xAxis: {
+                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    name: "OFO",
+                    type: 'bar',
+                    data: [2000, 6000, 4500, 3600, 3600, 7200, 800]
+                },
+                {
+                    name: "摩拜",
+                    type: 'bar',
+                    data: [1340, 2640, 6400, 8400, 3600, 9200, 6500]
+                },
+                {
+                    name: "小蓝",
+                    type: 'bar',
+                    data: [5750, 5670, 4660, 3450, 3450, 3440, 3340]
+                }
+            ]
+        }
+        return option
+    }
     render() {
         return (
             <div>
-                柱状图
+                <Card title="柱形图表之一">
+                    <ReactEcharts option={this.getOption()} theme='Imooc' style={{ height: 500 }}></ReactEcharts>
+                </Card>
+                <Card title="柱形图表之二" style={{ marginTop: 10 }}>
+                    <ReactEcharts option={this.getOption2()} theme='Imooc' style={{ height: 500 }}></ReactEcharts>
+                </Card>
             </div>
         );
     }
